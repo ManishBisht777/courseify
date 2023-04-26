cf470171d66863a5a0ee33e6062b1503b57552ed
manish bisht <manish205770@keshav.du.ac.in>
2023-04-26 12:42:12 +0530

    feat: navbar

---
+++ components/layout/MobileNav.tsx
@@ -0,0 +1,42 @@
+import { cn } from "@/lib/utils";
+import { NavItem } from "@/types";
+import Link from "next/link";
+import React from "react";
+import { Icons } from "../Icons";
+
+interface MobileNavProps {
+  items: NavItem[];
+}
+
+const MobileNav = ({ items }: MobileNavProps) => {
+  return (
+    <div
+      className={cn(
+        "fixed inset-0 top-16 z-50 grid h-[calc(100vh-4rem)] grid-flow-row auto-rows-max overflow-auto p-6 pb-32 shadow-md animate-in slide-in-from-bottom-80 md:hidden"
+      )}
+    >
+      <div className="relative z-20 grid gap-6 rounded-md bg-popover p-4 text-popover-foreground shadow-md">
+        <Link href="/" className="flex items-center space-x-2">
+          <Icons.logo />
+          <span className="font-bold">Courseify</span>
+        </Link>
+        <nav className="grid grid-flow-row auto-rows-max text-sm">
+          {items.map((item, index) => (
+            <Link
+              key={index}
+              href={item.disabled ? "#" : item.href}
+              className={cn(
+                "flex w-full items-center text-foreground/60 rounded-md p-2 text-sm font-medium hover:underline",
+                item.disabled && "cursor-not-allowed opacity-60"
+              )}
+            >
+              {item.title}
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default MobileNav;
---
+++ config/navbar.ts
@@ -0,0 +1,26 @@
+import { NavbarConfig } from "@/types";
+
+export const navbarConfig: NavbarConfig = {
+  mainNav: [
+    {
+      title: "Features",
+      href: "/#features",
+    },
+    {
+      title: "Support",
+      href: "/Support",
+    },
+    {
+      title: "Blog",
+      href: "/blog",
+    },
+    {
+      title: "Courses",
+      href: "/Courses",
+    },
+    {
+      title: "Documentation",
+      href: "/Docs",
+    },
+  ],
+};

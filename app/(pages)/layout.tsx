64d08af7efb664b68a513fb3488cca8cfb19a07d
manish bisht <manish205770@keshav.du.ac.in>
2023-04-26 11:31:35 +0530

    feat: fonts and root layout

---
+++ app/(pages)/layout.tsx
@@ -0,0 +1,35 @@
+import "../../styles/globals.css";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/toaster";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata = {
+  title: "Crouseify",
+  description: "Share your Knowledge in Public",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}
